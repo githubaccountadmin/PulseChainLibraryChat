8270661ec63da5c4ac5dda2818fb2de4a38c4a98
githubaccountadmin <148458819+githubaccountadmin@users.noreply.github.com>
2023-10-19 13:41:07 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
+// Initialize a post ID counter
+let postId = 0;
+
 // Get the form element
 const form = document.getElementById("userInputForm");
-
-// Get the tweets section element
-const tweetsSection = document.getElementById("tweets");
 
 // Listen for form submission
 form.addEventListener("submit", function(event) {
   event.preventDefault(); // Prevent the form from reloading the page
+
+  // Increment the post ID
+  postId++;
 
   // Get the content and post type from the form
   const content = document.getElementById("content").value;
   const postType = document.getElementById("postType").value;
 
   // Create a new element to hold the post
-  const newPost = document.createElement("p");
-  newPost.textContent = content;
+  const newPost = document.createElement("div");
 
-  // Decide where to add the new post
+  // Add a timestamp and ID to the post
+  const timestamp = new Date().toLocaleString();
+  newPost.innerHTML = `<strong>ID: ${postId}</strong> - <em>${timestamp}</em><br>${content}`;
+
+  // Decide where to add the new post based on its type
   const targetSection = document.getElementById(postType + "s");
   targetSection.appendChild(newPost);
 
