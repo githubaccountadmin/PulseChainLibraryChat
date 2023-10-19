4d79fe98e8c4612f39029dc1515afaf0c0cea881
githubaccountadmin <148458819+githubaccountadmin@users.noreply.github.com>
2023-10-19 16:27:41 +0000

    Update script.js

--- script.js
+++ script.js
@@ -3,41 +3,42 @@ const web3 = new Web3(Web3.givenProvider || 'https://rpc.pulsechain.com');
 
 // Function to post content to a specific section
 function postContent(section) {
-    const contentInput = document.getElementById(section + '-input');
-    const targetSection = document.getElementById(section + '-section');
-    const newContent = document.createElement('p');
+    const contentInput = document.getElementById(section + 'Input');
+    const targetSection = document.getElementById(section + 'List');
+    const newContent = document.createElement('li');
     newContent.innerText = contentInput.value;
     targetSection.appendChild(newContent);
 }
 
 // Function to check if connected to PulseChain
-function checkPulseChain(networkId) {
-    const networkDisplay = document.getElementById('network-display');
+async function checkPulseChain() {
+    const networkId = await web3.eth.net.getId();
+    const networkDisplay = document.getElementById('networkStatus');
 
     // PulseChain's Chain ID is 369
-    const pulseChainId = 369;
-
-    if (networkId === pulseChainId) {
+    if (networkId === 369) {
         networkDisplay.innerHTML = "Connected to PulseChain";
-        networkDisplay.style.color = "green";
     } else {
         networkDisplay.innerHTML = "Not connected to PulseChain";
-        networkDisplay.style.color = "red";
     }
 }
 
-// Event listeners for the buttons
-document.getElementById('tweets-button').addEventListener('click', () => postContent('tweets'));
-document.getElementById('stories-button').addEventListener('click', () => postContent('stories'));
-document.getElementById('books-button').addEventListener('click', () => postContent('books'));
+document.addEventListener('DOMContentLoaded', () => {
+    // Event listeners for the buttons
+    document.getElementById('tweetButton').addEventListener('click', () => postContent('tweet'));
+    document.getElementById('storyButton').addEventListener('click', () => postContent('story'));
+    document.getElementById('bookButton').addEventListener('click', () => postContent('book'));
 
-// Get and display the current network ID
-web3.eth.net.getId().then(checkPulseChain);
+    document.getElementById("connectButton").addEventListener("click", async () => {
+        await ethereum.request({ method: 'eth_requestAccounts' });
+        await checkPulseChain();
+    });
 
-// Listen for network changes and update the display accordingly
-web3.eth.net.isListening().then(() => {
-    window.ethereum.on('chainChanged', (chainId) => {
-        const networkId = parseInt(chainId.substring(2), 16);  // Convert hexadecimal to decimal
-        checkPulseChain(networkId);
+    // Initial network check
+    checkPulseChain();
+
+    // Listen for network changes and update the display accordingly
+    window.ethereum.on('chainChanged', async () => {
+        await checkPulseChain();
     });
 });
