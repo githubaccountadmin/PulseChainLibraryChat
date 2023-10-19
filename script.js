582690785926739b58b0a3365ecf78a656427353
githubaccountadmin <148458819+githubaccountadmin@users.noreply.github.com>
2023-10-19 15:58:44 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,43 @@
-// Initialize Web3
-let web3 = new Web3(Web3.givenProvider || "YOUR_RPC_URL_HERE");
+// Initialize web3 instance
+const web3 = new Web3(Web3.givenProvider || 'https://rpc.pulsechain.com');
 
-// Wallet Connection Function
-async function connectWallet() {
-  const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
-  const account = accounts[0];
-  console.log(`Connected account: ${account}`);
+// Function to post content to a specific section
+function postContent(section) {
+    const contentInput = document.getElementById(section + '-input');
+    const targetSection = document.getElementById(section + '-section');
+    const newContent = document.createElement('p');
+    newContent.innerText = contentInput.value;
+    targetSection.appendChild(newContent);
 }
 
-// Event Listener for Wallet Connection
-document.getElementById('connect-wallet').addEventListener('click', connectWallet);
+// Function to check if connected to PulseChain
+function checkPulseChain(networkId) {
+    const networkDisplay = document.getElementById('network-display');
 
-// Previous code
-document.addEventListener('DOMContentLoaded', function () {
-    let buttons = document.querySelectorAll('.post-button');
+    // PulseChain's Chain ID is 369
+    const pulseChainId = 369;
 
-    buttons.forEach(function (button) {
-        button.addEventListener('click', function (e) {
-            let target = e.target.dataset.target;
-            let message = document.getElementById(target + '-message').value;
-            let targetSection = document.getElementById(target + '-section');
+    if (networkId === pulseChainId) {
+        networkDisplay.innerHTML = "Connected to PulseChain";
+        networkDisplay.style.color = "green";
+    } else {
+        networkDisplay.innerHTML = "Not connected to PulseChain";
+        networkDisplay.style.color = "red";
+    }
+}
 
-            let newMessage = document.createElement('p');
-            newMessage.textContent = message;
-            targetSection.appendChild(newMessage);
-        });
+// Event listeners for the buttons
+document.getElementById('tweets-button').addEventListener('click', () => postContent('tweets'));
+document.getElementById('stories-button').addEventListener('click', () => postContent('stories'));
+document.getElementById('books-button').addEventListener('click', () => postContent('books'));
+
+// Get and display the current network ID
+web3.eth.net.getId().then(checkPulseChain);
+
+// Listen for network changes and update the display accordingly
+web3.eth.net.isListening().then(() => {
+    window.ethereum.on('chainChanged', (chainId) => {
+        const networkId = parseInt(chainId.substring(2), 16);  // Convert hexadecimal to decimal
+        checkPulseChain(networkId);
     });
 });
