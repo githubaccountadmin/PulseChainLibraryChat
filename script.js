db0eb33667b79db9b2b777a2a893db7ea943ed7e
githubaccountadmin <148458819+githubaccountadmin@users.noreply.github.com>
2023-10-19 14:09:54 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,19 @@
-// Initialize a post ID counter
-let postId = 0;
+document.addEventListener("DOMContentLoaded", () => {
+  const postButton = document.getElementById("postButton");
+  postButton.addEventListener("click", () => {
+    const postType = document.getElementById("postType").value;
+    const postContent = document.getElementById("postContent").value;
+    const timestamp = new Date().toISOString();
+    const postId = Math.random().toString(36).substr(2, 9);
 
-// Get the form element
-const form = document.getElementById("userInputForm");
+    const newPost = document.createElement("div");
+    newPost.innerHTML = `
+      <h2>${postType} - ID: ${postId}</h2>
+      <p>${postContent}</p>
+      <small>${timestamp}</small>
+    `;
 
-// Listen for form submission
-form.addEventListener("submit", function(event) {
-  event.preventDefault(); // Prevent the form from reloading the page
-
-  // Increment the post ID
-  postId++;
-
-  // Get the content and post type from the form
-  const content = document.getElementById("content").value;
-  const postType = document.getElementById("postType").value;
-
-  // Create a new element to hold the post
-  const newPost = document.createElement("div");
-
-  // Add a timestamp and ID to the post
-  const timestamp = new Date().toLocaleString();
-  newPost.innerHTML = `<strong>ID: ${postId}</strong> - <em>${timestamp}</em><br>${content}`;
-
-  // Decide where to add the new post based on its type
-  const targetSection = document.getElementById(postType + "s");
-  targetSection.appendChild(newPost);
-
-  // Clear the textarea
-  document.getElementById("content").value = "";
+    const targetSection = document.getElementById(postType.toLowerCase() + "s");
+    targetSection.appendChild(newPost);
+  });
 });
