111d8329e404fef4bb534966c66c4817ddd81455
githubaccountadmin <148458819+githubaccountadmin@users.noreply.github.com>
2023-10-21 12:27:20 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
+
     const web3 = new Web3(Web3.givenProvider || 'https://rpc.pulsechain.com');
-    let transactionCount = 10;
+    let transactionCount = 10;  // Default to fetching the last 10 transactions
 
     async function connectWallet() {
         const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
@@ -24,54 +25,49 @@ document.addEventListener('DOMContentLoaded', function() {
         contentInput.value = '';
     }
 
-   async function fetchTransactionData() {
-    const window = document.getElementById('transaction-data-window');
-    window.innerHTML = 'Fetching data...';
-    const apiEndpoint = 'https://scan.pulsechain.com/api?module=account&action=txlist&address=0x9Cd83BE15a79646A3D22B81fc8dDf7B7240a62cB&sort=desc';
+    async function fetchTransactionData() {
+        const window = document.getElementById('transaction-data-window');
+        window.innerHTML = 'Fetching data...';
 
-    try {
-        const response = await fetch(apiEndpoint);
-        const data = await response.json();
+        const apiEndpoint = 'https://scan.pulsechain.com/api?module=account&action=txlist&address=0x9Cd83BE15a79646A3D22B81fc8dDf7B7240a62cB&sort=desc';
 
-        console.log("Fetched data:", data);
-
-        const filteredData = data.result.filter(tx => tx.input !== '0x').slice(0, 10).map(tx => {
-            let decodedInput;
-            try {
-                decodedInput = web3.utils.hexToUtf8(tx.input);
-            } catch (e) {
-                console.error("Failed to decode input:", tx.input, e);
-                decodedInput = "Invalid UTF-8 data";  // This is where the error message is set
-            }
-
-            return {
-                from: tx.from,
-                input: decodedInput
-            };
-        });
-
-        console.log("Filtered data:", filteredData);
-
-        window.innerText = JSON.stringify(filteredData, null, 2);
-    } catch (error) {
-        console.error("Error details:", error.name, error.message);
-        window.innerHTML = `Error fetching data: ${error.name} - ${error.message}`;
+        try {
+            const response = await fetch(apiEndpoint);
+            const data = await response.json();
+            const filteredData = data.result.filter(tx => tx.input !== '0x').slice(0, transactionCount).map(tx => {
+                let decodedInput;
+                try {
+                    decodedInput = web3.utils.isHexStrict(tx.input) ? web3.utils.hexToUtf8(tx.input) : 'Not a hex string';
+                } catch (error) {
+                    console.error(`Error decoding hex to UTF-8 for tx.input: ${tx.input}`);
+                    console.error(error);
+                    decodedInput = 'Invalid UTF-8 data';
+                }
+                return {
+                    from: tx.from,
+                    input: decodedInput
+                };
+            });
+            window.innerText = JSON.stringify(filteredData, null, 2);
+        } catch (error) {
+            console.error("Error details:", error.name, error.message);
+            window.innerHTML = `Error fetching data: ${error.name} - ${error.message}`;
+        }
     }
-}
 
-    function displayTransactions(transactions) {
-        const window = document.getElementById('transactionDataWindow');
-        const filteredTransactions = transactions.filter(tx => tx.input !== '0x').map(tx => {
-            return `From: ${tx.from}\nMessage: ${web3.utils.hexToUtf8(tx.input)}`;
-        }).join('\n\n');
-        window.innerText = filteredTransactions;
+    function updateTransactionCount() {
+        const newCount = parseInt(document.getElementById('transactionCountInput').value);
+        if (!isNaN(newCount)) {
+            transactionCount = newCount;
+        }
     }
 
     document.getElementById('connectButton').addEventListener('click', connectWallet);
     document.getElementById('publishButton').addEventListener('click', postContent);
-    document.getElementById('loadMoreTransactionsButton').addEventListener('click', fetchTransactionData);
-    
-    // Initializing
+    document.getElementById('fetchDataButton').addEventListener('click', fetchTransactionData);
+    document.getElementById('transactionCountInput').addEventListener('input', updateTransactionCount);
+
     web3.eth.net.getId().then(checkPulseChain);
     fetchTransactionData();
+
 });
