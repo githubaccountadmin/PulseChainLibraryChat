fa039b7081a27141001e79bcb5f4fd488ce24f30
githubaccountadmin <148458819+githubaccountadmin@users.noreply.github.com>
2023-10-21 05:24:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     const web3 = new Web3(Web3.givenProvider || 'https://rpc.pulsechain.com');
-
-    async function connectWallet() {
-        try {
-            const accounts = await ethereum.enable();
-            const account = accounts[0];
-            console.log(`Connected to account ${account}`);
-        } catch (error) {
-            console.error('User denied account access');
-        }
-    }
-
-    function postContent(section) {
-        const contentInput = document.getElementById(section + 'Input');
-        const targetSection = document.getElementById(section + 'List');
+    
+    function postContent() {
+        const contentInput = document.getElementById('publishInput');
+        const targetSection = document.getElementById('publishList');
         const newContent = document.createElement('li');
         newContent.innerText = contentInput.value;
         targetSection.appendChild(newContent);
         contentInput.value = '';
     }
-
+    
     function checkPulseChain(networkId) {
         const networkDisplay = document.getElementById('networkStatus');
         const pulseChainId = 369;
+    
         if (networkId === pulseChainId) {
             networkDisplay.innerHTML = "Connected to PulseChain";
             networkDisplay.style.color = "green";
@@ -31,15 +22,15 @@ document.addEventListener('DOMContentLoaded', function() {
             networkDisplay.style.color = "red";
         }
     }
-
+    
     document.getElementById('connectButton').addEventListener('click', () => {
-        connectWallet();
+        // Add your code to connect the wallet here
     });
-
-    document.getElementById('tweetButton').addEventListener('click', () => postContent('tweet'));
-    document.getElementById('bookButton').addEventListener('click', () => postContent('book'));
-    document.getElementById('storyButton').addEventListener('click', () => postContent('story'));
-
+    
+    document.getElementById('publishButton').addEventListener('click', () => {
+        postContent();
+    });
+    
     async function fetchTransactionData() {
         const window = document.getElementById('transaction-data-window');
         window.innerHTML = 'Fetching data...';
@@ -53,10 +44,11 @@ document.addEventListener('DOMContentLoaded', function() {
             window.innerHTML = 'Error fetching data.';
         }
     }
-
+    
     document.getElementById('fetchDataButton').addEventListener('click', () => {
         fetchTransactionData();
     });
-
+    
     web3.eth.net.getId().then(checkPulseChain);
 });
+
