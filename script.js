ba07f84091615b3e49ce6f3e7d8b4bb1109809b4
githubaccountadmin <148458819+githubaccountadmin@users.noreply.github.com>
2023-10-21 03:39:57 +0000

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function postContent(section) {
     targetSection.appendChild(newContent);
 }
 
-// Function to fetch and display external transaction data
+// Function to fetch and display external transaction data as UTF-8 text
 async function fetchAndDisplayExternalTransactions(address) {
     const apiUrl = `https://scan.pulsechain.com/api?module=account&action=txlist&address=${address}&sort=desc`;
 
@@ -20,13 +20,16 @@ async function fetchAndDisplayExternalTransactions(address) {
 
         // Check if there are any transactions
         if (data.result && data.result.length > 0) {
-            // Display the raw input data from the latest transaction
+            // Get the raw input data from the latest transaction
             const latestTransaction = data.result[0];
             const inputData = latestTransaction.input;
 
-            // Display the input data in a window or element on your website
+            // Convert hexadecimal to UTF-8 text
+            const utf8Text = hexToUtf8(inputData);
+
+            // Display the UTF-8 text in a window or element on your website
             const dataWindow = document.getElementById('transaction-data-window');
-            dataWindow.textContent = inputData;
+            dataWindow.textContent = utf8Text;
         } else {
             // No transactions found
             console.log('No transactions found for the given address.');
@@ -36,7 +39,20 @@ async function fetchAndDisplayExternalTransactions(address) {
     }
 }
 
-// Call the function to fetch and display external transaction data for the specified address
+// Helper function to convert hexadecimal to UTF-8 text
+function hexToUtf8(hex) {
+    const hexString = hex.startsWith('0x') ? hex.slice(2) : hex;
+    const bytes = [];
+
+    for (let i = 0; i < hexString.length; i += 2) {
+        bytes.push(parseInt(hexString.substr(i, 2), 16));
+    }
+
+    const utf8Text = new TextDecoder('utf-8').decode(new Uint8Array(bytes));
+    return utf8Text;
+}
+
+// Call the function to fetch and display external transaction data as UTF-8 text
 fetchAndDisplayExternalTransactions('0x9Cd83BE15a79646A3D22B81fc8dDf7B7240a62cB');
 
 // Function to check if connected to PulseChain
