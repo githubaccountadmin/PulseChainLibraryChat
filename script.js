30631bb0be417ef4bc8d6b8d9df8111da2abe6fb
githubaccountadmin <148458819+githubaccountadmin@users.noreply.github.com>
2023-10-24 22:03:58 +0000

    Update script.js

--- script.js
+++ script.js
@@ -255,7 +255,7 @@ document.addEventListener('DOMContentLoaded', function() {
     
                         if (tag) {
                             decodedInput = decodedInput.replace(`*****(${tag})*****`, '');
-                            outputText += `<div class="transaction"><p>User: ${tx.from} - ${tag ? `${tag}: ` : 'Message'}</p><br><p>${decodedInput}</p></div>`;
+                            outputText += `<div class="transaction"><p>User: ${tx.from} - ${tag ? `${tag}: ` : 'Message'} ${decodedInput}</p></div>`;
                             console.log("Added .transaction class to element:", tx.from);
                         } else {
                             outputText += `User: ${tx.from}\nMessage: ${decodedInput.replace(/\*\*\*\*\*\(.*?\)\*\*\*\*\*/, '')}\n\n`;
