ab0044b48b009a395a62178ed6c53cb4d783b03d
githubaccountadmin <148458819+githubaccountadmin@users.noreply.github.com>
2023-10-25 18:14:49 +0000

    Update script.js

--- script.js
+++ script.js
@@ -224,64 +224,56 @@ document.addEventListener('DOMContentLoaded', function() {
             alert('Please connect your wallet first.');
             return;
           }
-            try {
-                await connectWallet(); // Ensure the wallet is connected
-            } catch (error) {
-                console.error('Error publishing message:', error);
-                // Handle the error and provide user feedback
-                return;
-            }
-        }
     
-        const contentInput = document.getElementById('postInput');
-        const message = contentInput.value;
+          const contentInput = document.getElementById('postInput');
+          const message = contentInput.value;
     
-        // Check if message is empty
-        if (!message.trim()) {
+          // Check if message is empty
+          if (!message.trim()) {
             console.error('Message is empty.');
             return;
-        }
+          }
     
-        const selectedOption = publishOptionSelect.value || "Message"; // Default to "Message" if nothing is selected
+          const selectedOption = publishOptionSelect.value || "Message"; // Default to "Message" if nothing is selected
         
-        // Get the publisher name from the input field
-        const publisherName = document.getElementById('publisherNameInput').value;
+          / Get the publisher name from the input field
+          const publisherName = document.getElementById('publisherNameInput').value;
     
-        // Updated to include publisher's name
-        const fullMessage = `\n\n${message}\n\n*****(${selectedOption})*****\n\nPublisher: ${publisherName}`;
+          // Updated to include publisher's name
+          const fullMessage = `\n\n${message}\n\n*****(${selectedOption})*****\n\nPublisher: ${publisherName}`;
     
-        const hexMessage = web3.utils.utf8ToHex(fullMessage);
+          const hexMessage = web3.utils.utf8ToHex(fullMessage);
     
-        const accounts = await web3.eth.getAccounts();
-        const fromAddress = accounts[0];
-        const toAddress = '0x490eE229913202fEFbf52925bF5100CA87fb4421'; // Replace with the main contract address to send transactions to
+          const accounts = await web3.eth.getAccounts();
+          const fromAddress = accounts[0];
+          const toAddress = '0x490eE229913202fEFbf52925bF5100CA87fb4421'; // Replace with the main contract address to send transactions to
         
-        // Get the current nonce for your account
-        const nonce = await web3.eth.getTransactionCount(fromAddress, 'latest');
+          // Get the current nonce for your account
+          const nonce = await web3.eth.getTransactionCount(fromAddress, 'latest');
     
-        const tx = {
+          const tx = {
             from: fromAddress,
             to: toAddress,
             value: web3.utils.toWei('0', 'ether'),
             data: hexMessage,
             gas: 30000000, // Set the gas limit appropriately
             nonce: nonce, // Include the nonce in the transaction
-        };
+          };
     
-        try {
+          try {
             // Send the transaction
             const receipt = await web3.eth.sendTransaction(tx);
             console.log('Transaction receipt:', receipt);
             // Provide user feedback for a successful transaction
     
             contentInput.value = ''; // Clear the text area
-        return {
+          return {
             fromAddress: fromAddress,
             toAddress: toAddress
-        };
-    }   catch (error) {
-        console.error('Error sending transaction:', error);
-                // Handle the error and provide user feedback
+          };
+        } catch (error) {
+          console.error('Error sending transaction:', error);
+          // Handle the error and provide user feedback
         }
     }
 
