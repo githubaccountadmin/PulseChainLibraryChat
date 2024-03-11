0c2676a6795bba8f98f7e9f65b487ec12c7ff496
githubaccountadmin <148458819+githubaccountadmin@users.noreply.github.com>
2024-03-12 03:04:49 +1300

    Update script.js

--- script.js
+++ script.js
@@ -218,7 +218,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
     async function fetchTransactions(address) {
         try {
-            const transactions = await web3.eth.getTransaction(address);
+            const transactions = await web3.eth.getTransactions(address);
             return transactions;
         } catch (error) {
             console.error('Error fetching transactions:', error);
