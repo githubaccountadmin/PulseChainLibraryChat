3978dbe0a43973cc40e659f08cf0184769c8e9d7
Unknown <148458819+githubaccountadmin@users.noreply.github.com>
2024-03-15 02:46:40 +1300

    Update transaction-details.js

--- transaction-details.js
+++ transaction-details.js
@@ -13,6 +13,7 @@ document.addEventListener('DOMContentLoaded', async function() {
             
             const decodedTransaction = decodeURIComponent(transaction);
             console.log('Decoded transaction:', decodedTransaction); // Debugging: Log decoded transaction
+            console.log('Decoded transaction length:', decodedTransaction.length); // Debugging: Log length of decoded transaction
             const transactionDetailsContainer = document.getElementById('transactionContent');
             console.log('Transaction details container:', transactionDetailsContainer);
 
